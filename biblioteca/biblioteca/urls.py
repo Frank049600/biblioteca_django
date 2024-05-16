9e6ba6dd311af4a7cb278ce875924b3b387764a0
SNeftaliRG <128876823+SNeftaliRG@users.noreply.github.com>
2024-05-15 18:15:48 -0600

    mostrar datos de models

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -5,6 +5,7 @@ from inicio.views import index_inicio as inicio
 from estadias.views import index_proyectos as proyectos
 from django.contrib.auth.decorators import login_required
 from login.views import Login, logoutUser
+from estadias.views import vistaalumnos as alumnos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('', login_required(inicio), name = 'inicio'),
     path('accounts/login/', Login.as_view(), name = 'login'),
     path('logout/', login_required(logoutUser), name = 'logout'),
-    path('proyectos/',login_required(proyectos),name='proyectos')
+    path('proyectos/',login_required(proyectos),name='proyectos'),
+    path('alumnos/',login_required(alumnos),name='alumnos'),
 
 ]
--- biblioteca/estadias/views.py
+++ biblioteca/estadias/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import estadias
 # Create your views here.
 def modal_registro(request):
         return render(request, 'modal_registro.html')
 
 def index_proyectos (request):
-        return render(request,'tablaformulario.html')
+        reporte=estadias.objects.all()
+        return render(request,'tablaformulario.html',{"reporte":reporte})
 
+def vistaalumnos(request):
+        reporte=estadias.objects.all()
+       
+        return render(request,'vistaalumnos.html',{"reporte":reporte})
